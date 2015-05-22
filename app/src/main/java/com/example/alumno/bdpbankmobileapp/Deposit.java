65ce7cb41302d1c0cab4a375251d01fdbd44bd70
kirspas <kirspas@hotmail.com>
2015-05-22 14:02:30 -0500

    test kiril

--- app/src/main/java/com/example/alumno/bdpbankmobileapp/Deposit.java
+++ app/src/main/java/com/example/alumno/bdpbankmobileapp/Deposit.java
@@ -35,6 +35,7 @@ public class Deposit extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_deposit, menu);
+
         return true;
     }
 
